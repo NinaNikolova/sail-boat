dbac0c06b7a186f8d11a01a71913c2776f2b70a8
nina <ninagbs@abv.bg>
2024-09-07 21:58:38 +0300

    corrected title

--- app/(tabs)/competent-crew/index.tsx
+++ app/(tabs)/competent-crew/index.tsx
@@ -9,7 +9,7 @@ const Competentcrew = () => {
         <View className='flex-1'>
             <AppGradient colors={["#2e1f58", "#54426b", "#a790af"]}>
                 <ScrollView showsVerticalScrollIndicator={false}>
-                    <Text className='text-zinc-50 text-3xl font-bold'>Day Skipper help</Text>
+                    <Text className='text-zinc-50 text-3xl font-bold'>Competent Crew help</Text>
                     <View>
                         {SKIPPER_GALLERY.map((g) => (
                             <SkipperGallery key={g.title} title={g.title} previews={g.data} />
