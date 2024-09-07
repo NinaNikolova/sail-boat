e1f1062eb6e8fc7546e96e0ad7c0dd3402b15411
nina <ninagbs@abv.bg>
2024-09-07 21:20:22 +0300

    corrected route

---
+++ app/(tabs)/competent-crew/index.tsx
@@ -0,0 +1,24 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import AppGradient from '@/components/AppGradient';
+import SKIPPER_GALLERY from '@/constants/skipper-gallary';
+import SkipperGallery from '@/components/SkipperGallery';
+
+const Competentcrew = () => {
+    return (
+        <View className='flex-1'>
+            <AppGradient colors={["#2e1f58", "#54426b", "#a790af"]}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <Text className='text-zinc-50 text-3xl font-bold'>Day Skipper help</Text>
+                    <View>
+                        {SKIPPER_GALLERY.map((g) => (
+                            <SkipperGallery key={g.title} title={g.title} previews={g.data} />
+                        ))}
+                    </View>
+                </ScrollView>
+            </AppGradient>
+        </View>
+    )
+}
+
+export default Competentcrew;
